b4edeeda9b5d7c227a47d549a0ff92b331f0398c
Islam Gamal <semsim2005@hotmail.com>
2012-07-01 12:12:50 +0200

    Initial Project upload
    Initial upload of project

---
+++ HasherDemo/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Hasher;
+
+namespace HasherDemoTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string stringToHash = "helloWorld";
+            var hasher = new StringHasher();
+
+            var md5Hash = hasher.MD5ComputeHash(stringToHash);
+            Console.WriteLine(md5Hash);
+            Console.WriteLine(md5Hash.Length);
+            Console.WriteLine(hasher.MD5VerifyHash(stringToHash, md5Hash));
+            md5Hash = hasher.MD5SaltComputeHash(stringToHash);
+            Console.WriteLine(md5Hash);
+            Console.WriteLine(md5Hash.Length);
+            Console.WriteLine(hasher.MD5SaltVerifyHash(stringToHash, md5Hash));
+
+            var sha256Hash = hasher.SHA256ComputeHash(stringToHash);
+            Console.WriteLine(sha256Hash);
+            Console.WriteLine(sha256Hash.Length);
+            Console.WriteLine(hasher.SHA256VerifyHash(stringToHash, sha256Hash));
+            sha256Hash = hasher.SHA256SaltComputeHash(stringToHash);
+            Console.WriteLine(sha256Hash);
+            Console.WriteLine(sha256Hash.Length);
+            Console.WriteLine(hasher.SHA256SaltVerifyHash(stringToHash, sha256Hash));
+            
+            var sha512Hash = hasher.SHA512ComputeHash(stringToHash);
+            Console.WriteLine(sha512Hash);
+            Console.WriteLine(sha512Hash.Length);
+            Console.WriteLine(hasher.SHA512VerifyHash(stringToHash, sha512Hash));
+            sha512Hash = hasher.SHA512SaltComputeHash(stringToHash);
+            Console.WriteLine(sha512Hash);
+            Console.WriteLine(sha512Hash.Length);
+            Console.WriteLine(hasher.SHA512SaltVerifyHash(stringToHash, sha512Hash));
+            
+            Console.ReadKey();
+        }
+    }
+}
